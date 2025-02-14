570837f9f9814942c3658a2adc49b22586faebcd
AlProsenak <al.prosenak@gmail.com>
2025-02-14 01:17:53 +0100

    Add database auto migration at application startup

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,19 @@
 import os
 
+from flask_migrate import upgrade
+
 from src import app_factory
 from src.router.ErrorHandler import error_handler_bp
 from src.router.VehicleRouter import vehicle_router_bp
 
 environment: str = os.environ.get('FLASK_ENV', 'local')
 app = app_factory.initialize(environment)
+
+# Auto apply migrations
+with app.app_context():
+    upgrade()
+
+# TODO: if development environment populate database with dummy data
 
 app.register_blueprint(vehicle_router_bp)
 app.register_blueprint(error_handler_bp)
